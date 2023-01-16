275d46534ab0b3a02796ee000cadeaf2a33b1449
abdullahelshoura <36606092+abdullahelshoura@users.noreply.github.com>
2023-01-16 09:52:17 +0200

    Delete om_hospital directory

--- om_hospital/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import patient
-from . import appointment
-from . import patient_tag
-from . import odoo_playground
-from . import res_config_settings
-from . import operation
