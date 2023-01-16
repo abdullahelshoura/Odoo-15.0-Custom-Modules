da90fa1cb7e3531e682cf11d7dcd9b6136e08a88
abdullahelshoura <36606092+abdullahelshoura@users.noreply.github.com>
2023-01-16 10:29:25 +0200

    Custom Module: add two fields to SO & Invoice Line
    Two new Custom Fields (margin) in percent and (discount_amount) that are calculated in price_subtotal in Sale Order (sale.order.line) model and reflects its value to Customer Invoice (account.move.line) model

---
+++ custom_margin_to_sale_order/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Margin in SO & Invoice Line",
+    'summary': """""",
+    'description': """""",
+    'author': "Abdullah Elshoura",
+    'website': "http://www.abdullahelshoura.com",
+    'version': '0.1',
+    'depends': ['sale', 'account'],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/sale_order_view.xml',
+        'views/account_move_view.xml',
+        # 'views/templates.xml',
+    ],
+    'demo': [],
+}
